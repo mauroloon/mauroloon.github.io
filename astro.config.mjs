ff8fddcbe511a29590aa4d4e653500e63339d71d
Mauroloon <mauricio.quezadagarcia@gmail.com>
2024-02-24 01:08:53 -0300

    deploy github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mauroloon.github.io'
+    site: 'https://mauroloon.github.io',
 });
