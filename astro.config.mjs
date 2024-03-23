e0467dd6821ec88367686034617f944b11fe20ee
mauroloon <mauricio.quezadagarcia@gmail.com>
2024-03-23 03:33:49 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+const LIVE_URL = 'https://mauroloon.github.io';
 export default defineConfig({
-    site: 'https://mauroloon.github.io',
+    site: LIVE_URL,
 });
